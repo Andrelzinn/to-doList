0914ff62dd4b9bde3ce720c19493ad47dd3a0ece
Andrelzinn <103343221+Andrelzinn@users.noreply.github.com>
2024-09-01 17:09:21 -0300

    Update index.php

--- ToDoList/public/index.php
+++ ToDoList/public/index.php
@@ -2,7 +2,7 @@
     session_start();
 
     if (!isset($_SESSION['user'])) {
-        header("Location: /ToDoList/login.html");
+        header("Location: /ToDoList/public/login.html");
         exit;
     }
 ?>
